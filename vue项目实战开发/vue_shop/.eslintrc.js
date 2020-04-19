e81c734d29bb22ee7a054f3293a0baee349ce221
suqh <suqh_wy@163.com>
2020-04-19 22:17:56 +0800

    登录功能代码提交

--- vue项目实战开发/vue_shop/.eslintrc.js
+++ vue项目实战开发/vue_shop/.eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+
+    env: {
+        node: true
+    },
+
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+        'no-dupe-keys': 0,
+        'import/no-duplicates': 0,
+        'eol-last': 0
+    },
+
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ]
 }
