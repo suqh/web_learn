a0c3c947d8116f64487aa90bb450b7f77076554b
suqh <suqh_wy@163.com>
2020-04-21 21:36:27 +0800

    完成了分类功能的开发

--- vue项目实战开发/vue_shop/src/main.js
+++ vue项目实战开发/vue_shop/src/main.js
@@ -7,6 +7,7 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
 axios.interceptors.request.use(config => {
@@ -18,6 +19,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
