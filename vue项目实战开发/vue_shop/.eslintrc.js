65d092577bcfb04a76efea7a5cdd64ce3c8d3c0c
suqh <suqh_wy@163.com>
2020-04-21 11:28:01 +0800

    完成用户列表功能的开发

--- vue项目实战开发/vue_shop/.eslintrc.js
+++ vue项目实战开发/vue_shop/.eslintrc.js
@@ -19,7 +19,13 @@ module.exports = {
         'no-debugger': 'off',
         'no-dupe-keys': 0,
         'import/no-duplicates': 0,
-        'eol-last': 0
+        'eol-last': 0,
+        'quote-props':0,
+        'no-trailing-spaces':0,
+        'indent': 0,
+        'vue/no-unused-vars':0,
+        'no-unused-vars': 0,
+        'no-useless-return':0
     },
 
     extends: [
